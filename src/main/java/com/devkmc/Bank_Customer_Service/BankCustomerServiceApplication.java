d66d94199ec9c53d45cf36a9d4c298c46cc13288
allcasa <allcasa@naver.com>
2024-11-01 13:22:58 +0900

    BankSystem

---
+++ src/main/java/com/devkmc/Bank_Customer_Service/BankCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.devkmc.Bank_Customer_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankCustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankCustomerServiceApplication.class, args);
+	}
+
+}
